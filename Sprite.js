6e52c784de2e4a079013c17205873ac3b5c534b7
xqy <2795143323@qq.com>
2025-02-16 23:34:38 +0800

    step 2 init

---
+++ Sprite.js
@@ -0,0 +1,32 @@
+class Sprite {
+    
+    constructor(config) {
+
+
+
+
+        /////image
+        this.image = new Image();
+        this.image.src=config.src
+
+
+
+
+
+
+
+
+
+
+
+
+      /**
+       * animation of a Sprite.
+       */
+      this.animations = config.animations || {
+        idleDown: [[0, 0]],
+      };
+      this.currentAnimation = config.currentAnimation || "idleDown";
+      this.currentAnimationFrame = 0;
+    }
+}
