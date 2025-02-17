9c95e6abd2facbe1b75ee341471235d5754a06e1
xqy <2795143323@qq.com>
2025-02-17 20:19:44 +0800

    step 2.1

--- Overworld.js
+++ Overworld.js
@@ -14,40 +14,21 @@ class Overworld {
       this.ctx.drawImage(image, 0, 0);
     };
     image.src = "/images/maps/DemoLower.png";
-    /////////////////////////////
-    const shadow = new Image();
-    shadow.onload = () => {
-      this.ctx.drawImage(
-        shadow,
-        0, //left cut
-        0, //right cut
-        32,
-        32, //width,height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    shadow.src = "/images/characters/shadow.png";
-    /////////////////////////////
-    console.log("init2");
-    const x = 5;
-    const y = 6;
-    const hero = new Image();
-    hero.onload = () => {
-      this.ctx.drawImage(
-        hero,
-        0, //left cut
-        0, //right cut
-        32,
-        32, //width,height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    hero.src = "/images/characters/people/hero.png";
+
+    /////some Game Objects
+    const hero = new GameObject({
+      x: 5,
+      y: 6,
+    });
+    const npc1 = new GameObject({
+      x: 7,
+      y: 9,
+      src: "/images/characters/people/npc1.png",
+    });
+
+    setTimeout(() => {
+      hero.sprite.draw(this.ctx);
+      npc1.sprite.draw(this.ctx);
+    }, 200);
   }
 }
--- Sprite.js
+++ Sprite.js
@@ -1,32 +1,39 @@
 class Sprite {
-    
-    constructor(config) {
+  constructor(config) {
+    /////image
+    this.image = new Image();
+    this.image.src = config.src;
+    this.image.onload = () => {
+      this.isLoaded = true;
+    };
+    //////shadow
+    this.shadow = new Image();
+    this.useShadow = true; //config.useShadow||false
 
-
-
-
-        /////image
-        this.image = new Image();
-        this.image.src = config.src;
-
-
-
-
-
-
-
-
-
-
-
-
-      /**
-       * animation of a Sprite.
-       */
-      this.animations = config.animations || {
-        idleDown: [[0, 0]],
-      };
-      this.currentAnimation = config.currentAnimation || "idleDown";
-      this.currentAnimationFrame = 0;
+    if (this.useShadow) {
+      this.shadow.src = "/images/characters/shadow.png";
     }
+    this.shadow.onload = () => {
+      this.isShadowLoaded = true;
+    };
+    /**
+     * animation of a Sprite.
+     */
+    this.animations = config.animations || {
+      idleDown: [[0, 0]],
+    };
+    this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentAnimationFrame = 0;
+
+    this.gameObject = config.gameObject;
+  }
+
+  ////draw
+  draw(ctx) {
+    const x = this.gameObject.x * 16 - 8;
+    const y = this.gameObject.y * 16 - 18;
+    this.isShadowLoaded &&
+      ctx.drawImage(this.shadow,  x, y);
+    this.isLoaded && ctx.drawImage(this.image, 0, 0, 32, 32, x, y, 32, 32);
+  }
 }
